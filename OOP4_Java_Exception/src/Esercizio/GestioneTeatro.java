436566b1a0564e0378c1c3e2336b997d4c5fe0bb
Francesco Di Iorio <francesco2000d@gmail.com>
2025-11-19 14:36:13 +0100

    modified sout after for

--- OOP4_Java_Exception/src/Esercizio/GestioneTeatro.java
+++ OOP4_Java_Exception/src/Esercizio/GestioneTeatro.java
@@ -13,8 +13,8 @@ public class GestioneTeatro {
 
         } catch (postoNonValidoException | postoGiaOccupatoException | postiTerminatiException  e) {
             System.out.println(e.getMessage());
+            stampaPostiDisponibili();
         }
-        stampaPostiDisponibili();
     }
 
     public static void stampaPostiDisponibili() {
